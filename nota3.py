8c1cd097c7bbf428ff2280a131296ffa76115e07
Natan Neto <natanael.neto@academico.ufpb.br>
2023-10-04 15:40:20 -0300

    Processar Grafo
    Função para criar matriz de adj, lista de adj a partir do grafo fornecido por .txt

---
+++ nota3.py
@@ -0,0 +1,44 @@
+def processar_grafo(arquivo):
+    # Abrir o arquivo para leitura
+    with open(arquivo, "r") as grafo:
+        # Ler o número de vértices do arquivo e converte pra inteiro a partir da primeira linha
+        num_vertices = int(grafo.readline())
+
+        # Inicializar a matriz de adjacência com zeros
+        matriz_adj = [[0] * num_vertices for _ in range(num_vertices)]
+
+        # Preencher a matriz de adjacência a partir do arquivo
+        for i in range(num_vertices):
+            linha = grafo.readline().split()
+            for j in range(num_vertices):
+                matriz_adj[i][j] = int(linha[j])
+
+        # Inicializar a lista de adjacência como um dicionário vazio
+        lista_adj = {}
+        # Preencher a lista de adjacência a partir da matriz de adjacência
+        for i in range(num_vertices):
+            lista_adj[i] = []
+            for j in range(num_vertices):
+                if matriz_adj[i][j] == 1:
+                    # Adicionar vértice vizinho (j) à lista de adjacência
+                    lista_adj[i].append(j)
+        
+        return matriz_adj, lista_adj, num_vertices
+    
+
+
+def main():
+    matriz_adj, lista_adj, num_vertices = processar_grafo("grafo.txt")
+
+    print("Matriz de adjacência:")
+    for i in range(num_vertices):
+        print(matriz_adj[i])
+
+    print("\nLista de adjacência:")
+    for i in range(num_vertices):
+        print(f"{i}: {lista_adj[i]}")
+    
+    
+
+if __name__ == "__main__":
+    main()
